173c25f86870cac6b8bebe5250d5efee0779376c
Kinoli7 <kinoli@hotmail.com>
2022-07-22 14:15:03 +0200

    Assignment 4

---
+++ Source/ActionRoguelike/Private/AI/SBTTask_Heal.cpp
@@ -0,0 +1,40 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "AI/SBTTask_Heal.h"
+
+#include "AIController.h"
+#include "SAttributeComponent.h"
+#include "GameFramework/Character.h"
+
+USBTTask_Heal::USBTTask_Heal()
+{
+	HealValue = 50;
+}
+
+EBTNodeResult::Type USBTTask_Heal::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
+{
+	AAIController* MyController = OwnerComp.GetAIOwner();
+	if (ensure(MyController))
+	{
+		ACharacter* MyPawn = Cast<ACharacter>(MyController->GetPawn());
+		if (MyPawn == nullptr)
+		{
+			return EBTNodeResult::Failed;
+		}
+
+		USAttributeComponent* AttributeComp = MyPawn->FindComponentByClass<USAttributeComponent>();
+		if (AttributeComp->GetActualHealth() <= 10.0f)
+		{
+			AttributeComp->ApplyHealthChange(MyPawn, 50.0f);
+			return EBTNodeResult::Succeeded;
+		}
+		else
+		{
+			return EBTNodeResult::Failed;
+		}
+		
+	}
+	
+	return EBTNodeResult::Failed; 
+}
---
+++ Source/ActionRoguelike/Public/AI/SBTTask_Heal.h
@@ -0,0 +1,25 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BehaviorTree/BTTaskNode.h"
+#include "SBTTask_Heal.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ACTIONROGUELIKE_API USBTTask_Heal : public UBTTaskNode
+{
+	GENERATED_BODY()
+
+	virtual EBTNodeResult::Type ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) override;
+
+protected:
+	UPROPERTY(EditAnywhere, Category="AI")
+	float HealValue;
+
+public:
+	USBTTask_Heal();
+};
